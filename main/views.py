3dca42718cd253b3f259ddcdf31af41450483e6e
sseunghoon <sseunghun99@naver.com>
2021-05-08 14:53:55 +0000

    form.html 제작 중

--- main/views.py
+++ main/views.py
@@ -4,8 +4,25 @@ from django.template import loader
 from .models import Question, Result, Choice
 
 def index(request):
-    context={}
-    template = loader.get_template('index.html')
-    return HttpResponse(template.render(context, request))
+    total_num = Result.objects.count()
+    army_num = Result.objects.filter(serial_num=0).count()
+    navy_num = Result.objects.filter(serial_num=1).count()
+    airforce_num = Result.objects.filter(serial_num=2).count()
     
+    context = {
+        'total_num': total_num,
+        'army_num': army_num,
+        'navy_num': navy_num,
+        'airforce_num': airforce_num,
+    }
+    return render(request, 'index.html', context)
+
+def form(request):
+    questions = Question.objects.all()
+    questions_count = Question.objects.all().count()
     
+    context = {
+        'questions':questions,
+        'questions_count':questions_count,
+    }
+    return render(request, 'form.html', context)
