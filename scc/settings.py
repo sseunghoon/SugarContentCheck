8a0d4083f96ea640104f8012a278ef626281d48d
sseunghoon <sseunghun99@naver.com>
2021-05-19 11:11:48 +0000

    heroku deploy

--- scc/settings.py
+++ scc/settings.py
@@ -81,13 +81,25 @@ WSGI_APPLICATION = 'scc.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'waby',
+#         'USER': 'scc',
+#         'PASSWORD': 'betterthanyesterday',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'waby',
+        'USER': 'waby',
+        'PASSWORD': 'betterthanyesterday',
+        'HOST': '0.0.0.0',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
